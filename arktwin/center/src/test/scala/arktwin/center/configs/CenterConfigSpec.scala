e097faf60bbe7a1cbe2c3ecf8141ae9e5e409c20
Takatomo Torigoe <piyo7@users.noreply.github.com>
2025-07-02 10:09:50 +0900

    Add comprehensive tests for CenterConfig and EdgeConfig

---
+++ arktwin/center/src/test/scala/arktwin/center/configs/CenterConfigSpec.scala
@@ -0,0 +1,142 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024-2025 TOYOTA MOTOR CORPORATION
+package arktwin.center.configs
+
+import arktwin.common.data.*
+import arktwin.common.util.LoggerConfigurator.LogLevel
+import cats.data.NonEmptyChain
+import org.scalatest.funspec.AnyFunSpec
+
+import scala.concurrent.duration.DurationInt
+
+class CenterConfigSpec extends AnyFunSpec:
+  describe("CenterConfig"):
+    describe("toJson"):
+      it("converts config to JSON string"):
+        val config = CenterConfig(
+          dynamic = DynamicCenterConfig(
+            atlas = AtlasConfig(
+              culling = AtlasConfig.Broadcast(),
+              routeTableUpdateInterval = 1.second
+            )
+          ),
+          static = StaticCenterConfig(
+            clock = ClockConfig(
+              start = ClockConfig.Start(
+                initialTime = ClockConfig.Start.Relative(VirtualDuration(0, 0)),
+                clockSpeed = 1.0,
+                condition = ClockConfig.Start.Schedule(MachineDuration(0, 0))
+              )
+            ),
+            runIdPrefix = "test",
+            host = "localhost",
+            port = 8080,
+            portAutoIncrement = false,
+            portAutoIncrementMax = 0,
+            logLevel = LogLevel.Info,
+            logLevelColor = true,
+            logSuppressionList = Seq.empty,
+            actorTimeout = 30.seconds,
+            subscribeBatchSize = 100,
+            subscribeBatchInterval = 100.millis,
+            subscribeBufferSize = 1000
+          )
+        )
+        val json = config.toJson
+
+        assert(
+          json == """{"dynamic":{"atlas":{"culling":{"type":"Broadcast"},"routeTableUpdateInterval":"1s"}},"static":{"actorTimeout":"30s","clock":{"start":{"clockSpeed":1.0,"condition":{"schedule":{"nanos":0,"seconds":0},"type":"Schedule"},"initialTime":{"relative":{"nanos":0,"seconds":0},"type":"Relative"}}},"host":"localhost","logLevel":{"type":"Info"},"logLevelColor":true,"logSuppressionList":[],"port":8080,"portAutoIncrement":false,"portAutoIncrementMax":0,"runIdPrefix":"test","subscribeBatchInterval":"100ms","subscribeBatchSize":100,"subscribeBufferSize":1000}}"""
+        )
+
+    describe("validated"):
+      it("returns valid config when all fields are valid"):
+        val config = CenterConfig(
+          dynamic = DynamicCenterConfig(
+            atlas = AtlasConfig(
+              culling = AtlasConfig.GridCulling(Vector3Enu(1, 1, 1)),
+              routeTableUpdateInterval = 1.second
+            )
+          ),
+          static = StaticCenterConfig(
+            clock = ClockConfig(
+              start = ClockConfig.Start(
+                initialTime = ClockConfig.Start.Absolute(VirtualTimestamp(0, 0)),
+                clockSpeed = 1.0,
+                condition = ClockConfig.Start.Schedule(MachineDuration(100, 0))
+              )
+            ),
+            runIdPrefix = "valid",
+            host = "localhost",
+            port = 8080,
+            portAutoIncrement = true,
+            portAutoIncrementMax = 10,
+            logLevel = LogLevel.Debug,
+            logLevelColor = false,
+            logSuppressionList = Seq("org.apache.pekko"),
+            actorTimeout = 60.seconds,
+            subscribeBatchSize = 50,
+            subscribeBatchInterval = 200.millis,
+            subscribeBufferSize = 500
+          )
+        )
+
+        assert(config.validated("test").isValid)
+
+      it("returns invalid with error messages when fields are invalid"):
+        val config = CenterConfig(
+          dynamic = DynamicCenterConfig(
+            atlas = AtlasConfig(
+              culling = AtlasConfig.GridCulling(Vector3Enu(0, -1, 1)),
+              routeTableUpdateInterval = 0.seconds
+            )
+          ),
+          static = StaticCenterConfig(
+            clock = ClockConfig(
+              start = ClockConfig.Start(
+                initialTime = ClockConfig.Start.Relative(VirtualDuration(0, 0)),
+                clockSpeed = -1.0,
+                condition = ClockConfig.Start.Schedule(MachineDuration(-1, 0))
+              )
+            ),
+            runIdPrefix = "",
+            host = "",
+            port = 0,
+            portAutoIncrement = false,
+            portAutoIncrementMax = -1,
+            logLevel = LogLevel.Error,
+            logLevelColor = true,
+            logSuppressionList = Seq.empty,
+            actorTimeout = 0.seconds,
+            subscribeBatchSize = 0,
+            subscribeBatchInterval = 0.seconds,
+            subscribeBufferSize = 0
+          )
+        )
+        val result = config.validated("test")
+
+        assert(result.isInvalid)
+        assert(
+          result.swap.getOrElse(NonEmptyChain.one("")).toChain.toVector.toSet == Set(
+            "test.dynamic.atlas.culling.gridCellSize.{x, y, z} must be > 0",
+            "test.dynamic.atlas.routeTableUpdateInterval must be > 0",
+            "test.static.clock.start.clockSpeed must be >= 0",
+            "test.static.clock.start.condition.schedule must be >= 0",
+            "test.static.runIdPrefix must not be empty",
+            "test.static.host must not be empty",
+            "test.static.port must be > 0",
+            "test.static.portAutoIncrementMax must be >= 0",
+            "test.static.actorTimeout must be > 0",
+            "test.static.subscribeBatchSize must be > 0",
+            "test.static.subscribeBatchInterval must be > 0",
+            "test.static.subscribeBufferSize must be > 0"
+          )
+        )
+
+  describe("CenterConfig$"):
+    describe("loadRawOrThrow"):
+      it("loads and resolves Config from actual config files without exceptions"):
+        CenterConfig.loadRawOrThrow()
+
+    describe("loadOrThrow"):
+      it("loads and validates CenterConfig from actual config files without exceptions"):
+        CenterConfig.loadOrThrow()
---
+++ arktwin/edge/src/test/scala/arktwin/edge/configs/EdgeConfigSpec.scala
@@ -0,0 +1,160 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024-2025 TOYOTA MOTOR CORPORATION
+package arktwin.edge.configs
+
+import arktwin.common.util.LoggerConfigurator.LogLevel
+import arktwin.edge.data.Vector3
+import cats.data.NonEmptyChain
+import org.scalatest.funspec.AnyFunSpec
+
+import scala.concurrent.duration.DurationInt
+
+class EdgeConfigSpec extends AnyFunSpec:
+  describe("EdgeConfig"):
+    describe("toJson"):
+      it("converts config to JSON string"):
+        val config = EdgeConfig(
+          dynamic = DynamicEdgeConfig(
+            coordinate = CoordinateConfig(
+              axis = AxisConfig(
+                xDirection = AxisConfig.Direction.East,
+                yDirection = AxisConfig.Direction.North,
+                zDirection = AxisConfig.Direction.Up
+              ),
+              centerOrigin = Vector3(0, 0, 0),
+              rotation = QuaternionConfig,
+              lengthUnit = CoordinateConfig.LengthUnit.Meter,
+              speedUnit = CoordinateConfig.SpeedUnit.MeterPerSecond
+            ),
+            culling = CullingConfig(
+              edgeCulling = true,
+              maxFirstAgents = 100
+            )
+          ),
+          static = StaticEdgeConfig(
+            edgeIdPrefix = "test",
+            host = "localhost",
+            port = 8081,
+            portAutoIncrement = false,
+            portAutoIncrementMax = 0,
+            logLevel = LogLevel.Info,
+            logLevelColor = true,
+            logSuppressionList = Seq.empty,
+            actorTimeout = 30.seconds,
+            endpointTimeout = 10.seconds,
+            clockInitialStashSize = 100,
+            publishBatchSize = 50,
+            publishBufferSize = 500
+          )
+        )
+        val json = config.toJson
+
+        assert(
+          json == """{"dynamic":{"coordinate":{"axis":{"xDirection":{"type":"East"},"yDirection":{"type":"North"},"zDirection":{"type":"Up"}},"centerOrigin":{"x":0.0,"y":0.0,"z":0.0},"lengthUnit":{"type":"Meter"},"rotation":{"type":"QuaternionConfig"},"speedUnit":{"type":"MeterPerSecond"}},"culling":{"edgeCulling":true,"maxFirstAgents":100}},"static":{"actorTimeout":"30s","clockInitialStashSize":100,"edgeIdPrefix":"test","endpointTimeout":"10s","host":"localhost","logLevel":{"type":"Info"},"logLevelColor":true,"logSuppressionList":[],"port":8081,"portAutoIncrement":false,"portAutoIncrementMax":0,"publishBatchSize":50,"publishBufferSize":500}}"""
+        )
+
+    describe("validated"):
+      it("returns valid config when all fields are valid"):
+        val config = EdgeConfig(
+          dynamic = DynamicEdgeConfig(
+            coordinate = CoordinateConfig(
+              axis = AxisConfig(
+                xDirection = AxisConfig.Direction.East,
+                yDirection = AxisConfig.Direction.North,
+                zDirection = AxisConfig.Direction.Up
+              ),
+              centerOrigin = Vector3(1, 2, 3),
+              rotation = EulerAnglesConfig(
+                angleUnit = EulerAnglesConfig.AngleUnit.Degree,
+                rotationMode = EulerAnglesConfig.RotationMode.Extrinsic,
+                rotationOrder = EulerAnglesConfig.RotationOrder.XYZ
+              ),
+              lengthUnit = CoordinateConfig.LengthUnit.Millimeter,
+              speedUnit = CoordinateConfig.SpeedUnit.KilometerPerHour
+            ),
+            culling = CullingConfig(
+              edgeCulling = false,
+              maxFirstAgents = 50
+            )
+          ),
+          static = StaticEdgeConfig(
+            edgeIdPrefix = "valid",
+            host = "0.0.0.0",
+            port = 9090,
+            portAutoIncrement = true,
+            portAutoIncrementMax = 10,
+            logLevel = LogLevel.Debug,
+            logLevelColor = false,
+            logSuppressionList = Seq("org.apache.pekko"),
+            actorTimeout = 60.seconds,
+            endpointTimeout = 20.seconds,
+            clockInitialStashSize = 200,
+            publishBatchSize = 100,
+            publishBufferSize = 1000
+          )
+        )
+
+        assert(config.validated("test").isValid)
+
+      it("returns invalid with error messages when fields are invalid"):
+        val config = EdgeConfig(
+          dynamic = DynamicEdgeConfig(
+            coordinate = CoordinateConfig(
+              axis = AxisConfig(
+                xDirection = AxisConfig.Direction.East,
+                yDirection = AxisConfig.Direction.East,
+                zDirection = AxisConfig.Direction.East
+              ),
+              centerOrigin = Vector3(0, 0, 0),
+              rotation = QuaternionConfig,
+              lengthUnit = CoordinateConfig.LengthUnit.Meter,
+              speedUnit = CoordinateConfig.SpeedUnit.MeterPerSecond
+            ),
+            culling = CullingConfig(
+              edgeCulling = true,
+              maxFirstAgents = -1
+            )
+          ),
+          static = StaticEdgeConfig(
+            edgeIdPrefix = "",
+            host = "",
+            port = 0,
+            portAutoIncrement = false,
+            portAutoIncrementMax = -1,
+            logLevel = LogLevel.Error,
+            logLevelColor = true,
+            logSuppressionList = Seq.empty,
+            actorTimeout = 0.seconds,
+            endpointTimeout = 0.seconds,
+            clockInitialStashSize = 0,
+            publishBatchSize = 0,
+            publishBufferSize = 0
+          )
+        )
+        val result = config.validated("test")
+
+        assert(result.isInvalid)
+        assert(
+          result.swap.getOrElse(NonEmptyChain.one("")).toChain.toVector.toSet == Set(
+            "test.dynamic.coordinate.axis.{x, y, z} must contain (East or West) and (North or South) and (Up or Down)",
+            "test.dynamic.culling.maxFirstAgents must be >= 0",
+            "test.static.edgeIdPrefix: must match pattern [0-9a-zA-Z\\-_]+",
+            "test.static.host must not be empty",
+            "test.static.port must be > 0",
+            "test.static.portAutoIncrementMax must be >= 0",
+            "test.static.actorTimeout must be > 0",
+            "test.static.endpointTimeout must be > 0",
+            "test.static.clockInitialStashSize must be > 0",
+            "test.static.publishBatchSize must be > 0",
+            "test.static.publishBufferSize must be > 0"
+          )
+        )
+
+  describe("EdgeConfig$"):
+    describe("loadRawOrThrow"):
+      it("loads and resolves Config from actual config files without exceptions"):
+        EdgeConfig.loadRawOrThrow()
+
+    describe("loadOrThrow"):
+      it("loads and validates EdgeConfig from actual config files without exceptions"):
+        EdgeConfig.loadOrThrow()
