625d859f9465a6aabe66cba36c77af1329685e2f
Takatomo Torigoe <piyo7@users.noreply.github.com>
2025-08-12 14:05:56 +0900

    Refactor EdgeConfigurator and Chart tests with improved structure and coverage

--- arktwin/edge/src/test/scala/arktwin/edge/test/ActorTestBase.scala
+++ arktwin/edge/src/test/scala/arktwin/edge/test/ActorTestBase.scala
@@ -2,10 +2,13 @@
 // Copyright 2024-2025 TOYOTA MOTOR CORPORATION
 package arktwin.edge.test
 
-import org.apache.pekko.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
-import org.scalatest.BeforeAndAfterAll
+import org.apache.pekko.actor.testkit.typed.scaladsl.{ActorTestKit, ScalaTestWithActorTestKit}
 import org.scalatest.funspec.AnyFunSpecLike
 
-trait ActorTestBase extends ScalaTestWithActorTestKit with AnyFunSpecLike with BeforeAndAfterAll:
+import scala.util.Using.Releasable
+
+trait ActorTestBase extends ScalaTestWithActorTestKit with AnyFunSpecLike:
+  given Releasable[ActorTestKit] = _.shutdownTestKit()
+
   override def beforeAll() =
     scribe.Logger.root.withMinimumLevel(scribe.Level.Error).replace()
